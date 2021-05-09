12090b70cbea19fd06777cb1dde2f1595e0821d3
NitaRay <superlightwing@gmail.com>
2021-05-09 18:37:06 +0700

    Set up Apollo & React

--- README.md
+++
@@ -1,1 +0,0 @@
-# ielts-practice-web
---
+++ ielts-practice-app/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+serviceWorker.unregister();
